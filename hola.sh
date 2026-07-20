a0be847c565205634a1af880edeb5599db5bfe5c
Aless177288 <valles.780@icloud.com>
2026-07-20 16:09:59 -0600

    mi primer jenkis

---
+++ hola.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "=================================="
+echo "Hola desde GitHub"
+echo "Pipeline funcionando"
+echo "Fecha:"
+date
+echo "Usuario:"
+whoami
+echo "=================================="
